f6caf1bf57cc49e1d600e747b393ce0dc87accfb
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-20 23:32:02 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,21 +9,46 @@ class Scraper
     doc = Nokogiri::HTML(open(url))
     all_information = doc.css("div.roster-cards-container")
     
-    students = {}
+    students = []
     
     all_information.each do |roster|
-      students = {
-      :name => roster.css("h4.student-name").text
-      :location => roster.css("p.student-location").text
-      :profile_url => roster.css("div.student-card a").attr('href').value
+      roster.css("div.student-card a").each do |student|
+      
+      student_hash = {
+      :name => student.css("h4.student-name").text,
+      :location => student.css("p.student-location").text,
+      :profile_url => student.attr("href")
       }
-      binding.pry
-    end
+        students << student_hash
+      end
+    end 
+    students
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    socials = doc.css("div.social-icon-container")
+    quote = doc.css("div.profile-quote").text
+    bio = doc.css("div.description-holder p").text
     
-  end
-
+    socials.each do |social|
+      social_sm = social.css("a").attr("href").value
+      socials = {}
+    if social_sm.include?("twitter")
+        socials[:twitter] = social_sm
+    elsif social_sm.include?("linked")
+        socials[:linkedin] = social_sm
+    elsif social_sm.include?("github")
+        socials[:github] = social_sm
+    else
+      socials[:blog] = social_sm
+    end
+      socials[:profile_quote] = quote
+      socials[:bio] = bio
+    end
+      socials
+    end
+    
+    
 end
 
