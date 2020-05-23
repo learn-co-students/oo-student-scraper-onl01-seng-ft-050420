6c98a120a79ee975a1c695371671c2805eb075e9
anette87 <anettem.rivera@gmail.com>
2020-05-23 15:34:28 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,12 +17,12 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.send("twitter=", attributes_hash[:twitter])
-    self.send("linkedin=", attributes_hash[:linkedin])
-    self.send("github=", attributes_hash[:github])
-    self.send("blog=", attributes_hash[:blog])
-    self.send("profile_quote=", attributes_hash[:profile_quote])
-    self.send("bio=", attributes_hash[:bio])
+    self.twitter = attributes_hash[:twitter]
+    self.linkedin = attributes_hash[:linkedin]
+    self.github = attributes_hash[:github]
+    self.blog = attributes_hash[:blog]
+    self.profile_quote = attributes_hash[:profile_quote]
+    self.bio = attributes_hash[:bio]
   end
 
 
